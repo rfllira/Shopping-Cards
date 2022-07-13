736c78c7f72b7eee7c5a1a871d06f70d60acd388
Rafael Lira <rafaelira347@gmail.com>
2022-07-13 10:04:21 -0300

    ajustes

--- Shopping Cards/helpers/fetchItem.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-const fetchItem = (id) => {
-	const idItem = id;
-	const API_URL = `https://api.mercadolibre.com/items/${idItem}`;
-  
-	return fetch(API_URL).then((response) => response.json()).then((data) => ({ sku: data.id, name: data.title, salePrice: data.price }));
-};
-
-if (typeof module !== "undefined") module.exports = fetchItem;
